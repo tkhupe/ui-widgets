5d8cca70b9163e000ffbd01bae01c94dcd2f311a
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-03-06 19:12:12 -0600

    done

---
+++ Unsolved/assets/js/script.js
@@ -0,0 +1,70 @@
+var formEl = $('#skills-form');
+var nameInputEl = $('#skill-name');
+var dateInputEl = $('#datepicker');
+var skillsListEl = $('#skills-list');
+
+var printSkills = function (name, date) {
+  var listEl = $('<li>');
+  var listDetail = name.concat(' on ', date);
+  listEl.addClass('list-group-item').text(listDetail);
+  listEl.appendTo(skillsListEl);
+};
+
+var handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  var nameInput = nameInputEl.val();
+  var dateInput = dateInputEl.val();
+
+  if (!nameInput || !dateInput) {
+    console.log('You need to fill out the form!');
+    return;
+  }
+
+  printSkills(nameInput, dateInput);
+
+  nameInputEl.val('');
+  dateInputEl.val('');
+};
+
+formEl.on('submit', handleFormSubmit);
+
+// Add Autocomplete widget here
+//
+$( function() {
+  var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+  ];
+  
+  nameInputEl.autocomplete({
+    source: availableTags
+  });
+} );
+
+// Add Datepicker widget here
+//
+
+$( function() {
+  $( "#datepicker" ).datepicker();
+} );
